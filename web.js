14904038401e11e96882d1d8edcbc924282c2eaa
Molood <molood@gmail.com>
2013-07-16 11:36:58 +0000

    updated web.js to read from index.html

--- web.js
+++ web.js
@@ -2,9 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(256);
+buf = fs.readFileSync('index.html', 'utf8');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   response.send (buf.toString('utf8', 0, buf.length));
 });
+
+//app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
