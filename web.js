8d24f566779ca2255fdbd33a7a93f331cfe99f7c
Molood <molood@gmail.com>
2013-07-16 12:02:46 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,15 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(256);
-buf = fs.readFileSync('index.html', 'utf8');
 app.get('/', function(request, response) {
-   response.send (buf.toString('utf8', 0, buf.length));
-});
-
-//app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
-//});
+   var fs = require('fs');
+   var buf = new Buffer();
+   buf = fs.readFileSync('index.html', 'utf8');
+   response.send (buf.toString('utf8', fs.readFileSync('index.html')));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
