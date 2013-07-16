6a62d25e9257fc277cb5c22d95208f8e9cd8659a
Molood <molood@gmail.com>
2013-07-16 12:11:02 +0000

    Update web.js for buffer init

--- web.js
+++ web.js
@@ -4,9 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    var fs = require('fs');
-   var buf = new Buffer();
-   buf = fs.readFileSync('index.html', 'utf8');
-   response.send (buf.toString('utf8', fs.readFileSync('index.html')));
+   var buf = new Buffer (fs.readFileSync('index.html'), 'utf-8');
+   response.send (buf.toString();
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
