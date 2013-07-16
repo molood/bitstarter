f3e265ce26939f8a8eb547d6528f20aacd498f77
Molood <molood@gmail.com>
2013-07-16 13:03:10 +0000

    Another update made to web.js

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
    var fs = require('fs');
    var buf = new Buffer (fs.readFileSync('index.html'), 'utf-8');
-   response.send (buf.toString();
+   response.send (buf.toString('utf-8'));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
